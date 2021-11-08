46f10168d0a4f013dedc5abe2a0914c5f0ff4b5a
Jason Rivera <jp_rivera_@hotmail.com>
2021-11-07 17:57:53 -0800

    Display folder contents

--- ServerThread.cpp
+++ ServerThread.cpp
@@ -4,6 +4,10 @@
 #include <pthread.h>
 #include <exception>
 #include <string>
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <filesystem>
 #include "ServerThread.hpp"
 #include "HttpResponse.hpp"
 #include "UploadServlet.hpp"
@@ -53,16 +57,48 @@ void *ServerThread::run(void *arg) {
     // TODO: this part needs to go to response write
     //doGet?
 //    char buf2 = '\0';
-//    char responseHTML[1024] = "<!DOCTYPE html>\r\n"
-//    "<html>\r\n<head>\r\n<title>File Upload Form</title>\r\n</head>\r\n"
-//    "<body>\r\n<h1>Upload file</h1>\r\n"
-//    "<form method=\"POST\" action=\"**REDIRECT**\"enctype=\"multipart/form-data\">\r\n"
-//                                                          "<input type=\"file\" name=\"fileName\"/><br/><br/>\r\n"
-//                                                          "Caption: <input type=\"text\" name=\"caption\"<br/><br/>\r\n<br />\n"
-//                                                          "Date: <input type=\"date\" name=\"date\"<br/><br/>\r\n<br/>\n"
-//                                                          "<input type=\"submit\" value=\"Submit\"/>\r\n</form>\r\n</body>\r\n</html>\r\n";
-//    response.setContentType("text/html");
-//    strcpy(&buf2, responseHTML);
+
+    string LF = "\r\n";
+    time_t timetoday;
+    time(&timetoday);
+    string string1 = asctime(localtime(&timetoday));
+
+    string content =
+            "<!DOCTYPE html>\r\n"
+            "<html>\r\n<head>\r\n<title>File Upload Form</title>\r\n</head>\r\n"
+            "<body>\r\n<h1>Upload file</h1>\r\n"
+            "<form method=\"POST\" action=\"**REDIRECT**\"enctype=\"multipart/form-data\">\r\n"
+            "<input type=\"file\" name=\"fileName\"/><br/><br/>\r\n"
+            "Caption: <input type=\"text\" name=\"caption\"<br/><br/>\r\n<br />\n"
+            "Date: <input type=\"date\" name=\"date\"<br/><br/>\r\n<br/>\n"
+            "<input type=\"submit\" value=\"Submit\"/>\r\n</form>\r\n";
+
+    namespace fs = std::filesystem;
+    string path = IMAGES_FOLDER;
+    for(const auto& entry : fs::directory_iterator(path)) {
+        content += entry.path();
+        content += "<br/>";
+    }
+
+    content += "</body>\r\n</html>\r\n";
+    //    response.setContentType("text/html");
+    size_t contentLength = content.size() * sizeof(char);
+
+//    auto now = chrono::system_clock::now();
+//    responseStr += "Date: " + ctime(&now);
+
+    string responseStr = "HTTP/1.1 200 ok" + LF;
+    responseStr += "Date: Sun, 7 Nov 2021 15:11:00 GMT" + LF;
+    responseStr += "Server: Custom C++ server" + LF;
+    responseStr += "Connection: close" + LF;
+    responseStr += "Content-Length: " + to_string(contentLength) + LF;
+    responseStr += "Content-Type: text/html" + LF;
+    responseStr += LF;
+    responseStr += content;
+
+    cout << responseStr << endl;
+    const char *asCharArray = responseStr.c_str();
+    strcpy(outBuffer, asCharArray);
 
     // write buffer to the end.
     if ((readLength = write(clientsock, outBuffer, 1024)) < 0) {
